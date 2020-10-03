9cd8a0e20c2ada89d7fdaea7189ca4e8a8994eb6
Ru-Two <darjirutu@gmail.com>
2020-10-03 15:16:31 -0400

    Created Trait class

---
+++ src/utilities/Trait.java
@@ -0,0 +1,11 @@
+package utilities;
+
+public class Trait {
+    private String name;
+    private String description;
+
+    public Trait(String n, String d){
+        name = n;
+        description = d;
+    }
+}
