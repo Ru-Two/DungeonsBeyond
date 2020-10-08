a1be33e72ab0fbb61647f2740aef1f98ca654ce5
Ru-Two <darjirutu@gmail.com>
2020-10-08 17:46:13 -0400

    updated Item class

--- src/items/Item.java
+++ src/items/Item.java
@@ -1,6 +1,25 @@
 package items;
 
 public abstract class Item {
+    protected String name;
+    protected int cost;
+    protected int weight;
 
+    public Item(String n, int c, int w){
+        name = n;
+        cost = c;
+        weight = w;
+    }
 
+    public int getCost() {
+        return cost;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public int getWeight() {
+        return weight;
+    }
 }
