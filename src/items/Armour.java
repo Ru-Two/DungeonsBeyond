dc5a5fb8ba44174582bfa9490e092da8675adfac
Abdulaziz Khan <abdulazizkhan75@gmail.com>
2020-10-08 18:17:58 -0400

    Update Armour.java

--- src/items/Armour.java
+++ src/items/Armour.java
@@ -1,13 +1,32 @@
 package items;
 
 public class Armour extends Item{
-    public Armour(int AC, int Strength, Boolean Stealth ){
+    private int ac;
+    private int str;
+    private int modifier;
+    private boolean stl;
+    public Armour(String n, int base_ac, int mod, int Strength, Boolean Stealth, int c, int w){
         super(n,c,w);
-
-
-
-
+        ac = base_ac;
+        str = Strength;
+        modifier = mod;
+        stl = Stealth;
 
     }
 
+    public int getAc() {
+        return ac;
+    }
+
+    public int getModifier() {
+        return modifier;
+    }
+
+    public int getStr() {
+        return str;
+    }
+
+    public boolean StealthDisAdv() {
+        return stl;
+    }
 }
