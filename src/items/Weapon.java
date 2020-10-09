ae0bb47c5fcc6e1c073fa2d6b360ff2b3faba23d
Ru-Two <darjirutu@gmail.com>
2020-10-09 12:55:35 -0400

    Updated Weapon class. Added utility functions and weapon types

--- src/items/Weapon.java
+++ src/items/Weapon.java
@@ -1,29 +1,55 @@
 package items;
 
-import utilities.Dice;
+import utilities.*;
+
+import utilities.*;
+import static utilities.Utilities.*;
 
 public class Weapon extends Item{
-    private Dice dmg;
+    private Dice dmgDice;
     private String dmgType;
     private String properties;
+    private int weaponType;
+    private int dmgModifer;
 
     // add Dice class and make dmgType of that class
-    public Weapon(String n, Dice dmg, String dmgType, String properties, int c, int w){
+    public Weapon(String n, int weaponType, Dice dmgDice, String dmgType, String properties, int c, int w){
         super(n,c,w);
-        this.dmg = dmg;
+        this.weaponType = weaponType;
+        this.dmgDice = dmgDice;
         this.dmgType = dmgType;
         this.properties = properties;
+
+        if (weaponType == SIMPLE_RANGED || weaponType == MARTIAL_RANGED) { dmgModifer = DEX; }
+        if (weaponType == SIMPLE_MELEE || weaponType == MARTIAL_MELEE) { dmgModifer = STR; }
+
     }
 
-    public Weapon(String n, String dmgDice, String dmgType, String properties, int c, int w){
+    public Weapon(String n, int weaponType, String dmgDice, String dmgType, String properties, int c, int w){
         super(n,c,w);
-        dmg = new Dice(dmgDice);
+        this.weaponType = weaponType;
+        this.dmgDice = new Dice(dmgDice);
         this.dmgType = dmgType;
         this.properties = properties;
+        
+        if (weaponType == SIMPLE_RANGED || weaponType == MARTIAL_RANGED) { dmgModifer = DEX; }
+        if (weaponType == SIMPLE_MELEE || weaponType == MARTIAL_MELEE) { dmgModifer = STR; }
     }
 
-    public Dice getDmg(){
-        return dmg;
+    public void setDmgModifer(int mod){
+        dmgModifer = mod;
+    }
+
+    public int dealBaseDamage(){
+        return dmgDice.roll();
+    }
+
+    public int[] dealBaseDamage_GrabArr(){
+        return dmgDice.roll_GrabArr();
+    }
+
+    public Dice getDmgDice(){
+        return dmgDice;
     }
 
     public String getDmgType() {
@@ -32,6 +58,14 @@ public class Weapon extends Item{
 
     public String getProperties() {
         return properties;
+    }
+
+    public int getWeaponType(){
+       return weaponType;
+    }
+
+    public String toString(){
+        return name + " - " + dmgDice + " " + dmgType;
     }
 
 }
