24c505b2973a6974228e9eea49fad6e7ab708328
Andrew Ferasol <ajferasol@gmail.com>
2020-10-12 20:17:26 -0400

    Add files via upload

--- src/main/DungeonsBeyond.java
+++ src/main/DungeonsBeyond.java
@@ -11,6 +11,6 @@ public class DungeonsBeyond {
     }
 
     public static void main(String[] args){
-        System.out.println(bardicInspiration);
+        System.out.println(divineIntervention);
     }
 }
