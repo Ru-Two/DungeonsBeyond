54b4240fa8a98b5b46af715722770c85e7a121e8
Ru-Two <darjirutu@gmail.com>
2020-10-16 14:48:13 -0400

    Created character sheet class

---
+++ src/main/CharacterSheet.java
@@ -0,0 +1,14 @@
+package main;
+
+import character.*;
+import utilities.*;
+import javax.swing.*;
+import java.awt.*;
+
+public class CharacterSheet {
+    private CharacterInfo character;
+    
+    public CharacterSheet(){
+
+    }
+}
