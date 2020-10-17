5fef1c34231b4409637465dcc7eef368bc978b50
Ru-Two <darjirutu@gmail.com>
2020-10-16 22:02:42 -0400

    Added base JPanels to CharacterSheet

--- src/main/CharacterSheet.java
+++ src/main/CharacterSheet.java
@@ -7,8 +7,32 @@ import java.awt.*;
 
 public class CharacterSheet {
     private CharacterInfo character;
-    
-    public CharacterSheet(){
+    private Dimension screenSize;
+    private Dimension sheetSize;
+    private Dimension sheetSizeRatio = new Dimension(17, 22);
 
+    private JPanel headerPanel;
+    private JPanel abilityScorePanel;
+    private JPanel inspProfPanel;
+    private JPanel savingThrowPanel;
+    private JPanel skillPanel;
+    private JPanel healthPanel;
+    private JPanel combatPanel;
+    private JPanel personalInfoPanel;
+    private JPanel languagePanel;
+    private JPanel featureAndTraitPanel;
+    private JPanel attackAndSpellcastingPanel;
+    private JPanel equipmentPanel;
+
+    // TODO: set base graphics things here
+    public CharacterSheet(){
+        character = new CharacterInfo();
+        screenSize = Toolkit.getDefaultToolkit().getScreenSize();
+        sheetSize = new Dimension((int)((screenSize.getHeight()-50)*(sheetSizeRatio.getWidth()/sheetSizeRatio.getHeight())), (int)screenSize.getHeight()-50);
+    }
+
+    public CharacterSheet(String characterName){
+        this();
+        character.setCharacterName(characterName);
     }
 }
