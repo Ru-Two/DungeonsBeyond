54e3dc706384b3e70a72fb485a75db56a5228297
Ru-Two <darjirutu@gmail.com>
2020-10-26 16:10:42 -0400

    Created SheetComponentPanel

---
+++ src/main/SheetComponentPanel.java
@@ -0,0 +1,38 @@
+package main;
+
+import javax.swing.*;
+import java.awt.*;
+import java.awt.image.*;
+import java.util.*;
+
+public class SheetComponentPanel extends JPanel {
+
+    ArrayList<DBImageComponent> images;
+    
+
+    public SheetComponentPanel(){
+        setLocation(0,0);
+        setSize(100,100);
+    }
+
+    public SheetComponentPanel(int x, int y){
+        this();
+        setLocation(x,y);
+    }
+
+    public SheetComponentPanel(int x, int y, int width, int height){
+        this();
+        setLocation(x,y);
+        setSize(width, height);
+    }
+
+    public void addImage(BufferedImage img){
+        addImage(img, 0, 0, getWidth(), getHeight());
+    }
+
+    public void addImage(BufferedImage img, int x, int y, int width, int height){
+
+    }
+
+
+}
