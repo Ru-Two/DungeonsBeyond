d0a26b359d8c1d467644a74ebf10ad83d52e2abf
Ru-Two <darjirutu@gmail.com>
2020-10-26 20:05:48 -0400

    Minor changes

--- src/main/DBTextAreaComponent.java
+++ src/main/DBTextAreaComponent.java
@@ -1,18 +1,24 @@
 package main;
 
 import javax.swing.*;
+import java.awt.*;
 
 
 public class DBTextAreaComponent extends JTextArea {
 
     public DBTextAreaComponent(){
-        super("",   1, 30);
+        super("",4,30);
         setLocation(0,0);
         setSize(100,50);
     }
 
     public DBTextAreaComponent(int width, int height){
         this();
-        
+        setSize(width, height);
+    }
+
+    @Override
+    public void paintComponent(Graphics g){
+        super.paintComponent(g);
     }
 }
