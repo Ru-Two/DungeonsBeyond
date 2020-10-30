04a5e17b8c97a0430d8d4b0dc6503327c267070a
Ru-Two <darjirutu@gmail.com>
2020-10-29 22:41:40 -0400

    Added PreloadedImages class. Might not use

---
+++ src/utilities/PreloadedImages.java
@@ -0,0 +1,10 @@
+package utilities;
+
+import main.DungeonsBeyond;
+
+import java.awt.image.BufferedImage;
+
+public final class PreloadedImages {
+    public static final BufferedImage abilityScores_png = DungeonsBeyond.getImage("src/graphics/abilityScores2.png");
+    public static final BufferedImage basicSheet = DungeonsBeyond.getImage("src/graphics/basic_sheet.png");
+}
