ec9a9f40778e189c50bea141c4a9ffcfc3e61516
Ru-Two <darjirutu@gmail.com>
2020-10-29 20:07:53 -0400

    Added fonts to text area

--- src/main/DBTextAreaComponent.java
+++ src/main/DBTextAreaComponent.java
@@ -6,19 +6,29 @@ import java.awt.*;
 
 public class DBTextAreaComponent extends JTextArea {
 
+    private Font font;
+
     public DBTextAreaComponent(){
         super("",4,30);
         setLocation(0,0);
         setSize(100,50);
+        font = new Font("Courier Regular", Font.PLAIN, 20);
     }
 
-    public DBTextAreaComponent(int width, int height){
+    public DBTextAreaComponent(int x, int y, int width, int height){
         this();
         setSize(width, height);
+        setLocation(x, y);
+    }
+
+    public void setFont(Font f){
+        font = f;
+        super.setFont(font);
     }
 
     @Override
     public void paintComponent(Graphics g){
         super.paintComponent(g);
+        g.setFont(font);
     }
 }
