c0ea367a0b0c6cb04639505388d3c15b10ce2482
Ru-Two <darjirutu@gmail.com>
2020-10-29 20:09:18 -0400

    Added MouseListener

--- src/main/SheetListPanel.java
+++ src/main/SheetListPanel.java
@@ -4,10 +4,11 @@ import character.*;
 import utilities.*;
 import javax.swing.*;
 import java.awt.*;
+import java.awt.event.*;
 import java.awt.image.*;
 import java.util.ArrayList;
 
-public class SheetListPanel extends JPanel {
+public class SheetListPanel extends JPanel implements MouseListener {
 
     private static final int BUTTON_SPACE = 20;
     private static final Color SELECT_COLOR = new Color(0,100,255, 50);
@@ -18,16 +19,19 @@ public class SheetListPanel extends JPanel {
     private DBButton newSheetButton;
     private int selectedSheet;
 
+    private boolean addSheetFlag;
 
     public SheetListPanel(){
         sheetButtons = new ArrayList<DBButton>();
-        selectedSheet = 0;
+        selectedSheet = -1;
         setLocation(0,0);
         setSize(100,100);
         BUTTON_SIZE = new Dimension(50,50);
         newSheetButton = new DBButton(getNextButtonBounds());
         //newSheetButton.setImage(DungeonsBeyond.getImage("src/graphics/add_sheet.png"));
         newSheetButton.setColor(new Color(5,250,20,60));
+        addSheetFlag = false;
+        addMouseListener(this);
     }
 
     public SheetListPanel(int x, int y, int wid, int hei){
@@ -46,15 +50,16 @@ public class SheetListPanel extends JPanel {
     }
 
     public int selectSheet(int index){
+        if (selectedSheet != -1) sheetButtons.get(selectedSheet).unSelect();
         selectedSheet = index;
+        sheetButtons.get(selectedSheet).select();
         return index;
     }
 
     public int selectSheet(int x, int y){
         for (int i = 0; i < sheetButtons.size(); i++){
             if (sheetButtons.get(i).contains(x, y)){
-                selectedSheet = i;
-                return selectedSheet;
+                return selectSheet(i);
             }
         }
         return -1;
@@ -83,14 +88,46 @@ public class SheetListPanel extends JPanel {
         return rect;
     }
 
+    public int getSelectedSheet(){
+        return selectedSheet;
+    }
+
+    public boolean checkAddSheetFlag(){
+        if (addSheetFlag){
+            addSheetFlag = false;
+            return true;
+        }
+        return false;
+    }
+
+    public void mousePressed(MouseEvent e) {}
+
+    public void mouseReleased(MouseEvent e) {}
+
+    public void mouseEntered(MouseEvent e) {}
+
+    public void mouseExited(MouseEvent e) {}
+
+    public void mouseClicked(MouseEvent e) {
+        if (e.getButton() == MouseEvent.BUTTON1){
+
+            if (newSheetButtonIsClicked(e.getPoint())) {
+                //addSheet();
+                addSheetFlag = true;
+            }
+            int tmpIndex = selectSheet(e.getPoint());
+            if (tmpIndex != -1){
+                selectSheet(tmpIndex);
+                System.out.println("Sheet " +tmpIndex+ " selected");
+            }
+        }
+    }
+
     @Override
     public void paintComponent(Graphics g){
         //allSheets.get(selectedSheet).paintComponent(g);
         for (int i = 0; i < sheetButtons.size(); i++){
             sheetButtons.get(i).drawButton(g, this);
-            if (i == selectedSheet){
-                sheetButtons.get(i).drawButtonSelected(g, this);
-            }
         }
         newSheetButton.drawButton(g, this);
     }
