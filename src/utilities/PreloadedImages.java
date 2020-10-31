924876a298c2e851dd277df00a9ef5172965d1b7
Ru-Two <darjirutu@gmail.com>
2020-10-31 17:08:12 -0400

    Added to final class

--- src/utilities/PreloadedImages.java
+++ src/utilities/PreloadedImages.java
@@ -12,10 +12,17 @@ public final class PreloadedImages {
     public static final Dimension windowSize = new Dimension((int)sheetSize.getHeight(), (int)sheetSize.getHeight());
     public static final double windowUnit = (sheetSize.getWidth()/8.5);
 
-    public static final BufferedImage layoutSheet_unscaled = DungeonsBeyond.getImage("src/graphics/basic_sheet.png");
+    public static final BufferedImage layoutSheet_unscaled = DungeonsBeyond.getImage("src/graphics/basic_sheet2.png");
     public static final double layoutSheetUnit = (layoutSheet_unscaled.getWidth()/8.5);
-    public static final Image layoutSheet = layoutSheet_unscaled.getScaledInstance((int)sheetSize.getWidth(), (int)sheetSize.getHeight(), Image.SCALE_DEFAULT);
+    public static final Image layoutSheet = layoutSheet_unscaled.getScaledInstance((int)sheetSize.getWidth(), (int)sheetSize.getHeight(), Image.SCALE_SMOOTH);
     //public static final Image layoutSheet = layoutSheet_unscaled;
+
+    //Positions of everything on the sheet
+    public static final Rectangle player_name_position = new Rectangle(119, 146, 570, 65);
+
+
+    //Different font styles for different fields
+    public static final Font player_name_font = new Font("Verdana", Font.PLAIN, (int)(windowUnit/4.2));
 
     public static final BufferedImage abilityScores_png = DungeonsBeyond.getImage("src/graphics/abilityScores2.png");
     public static final BufferedImage basicSheet = DungeonsBeyond.getImage("src/graphics/basic_sheet.png");
