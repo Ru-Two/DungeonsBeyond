49e63f87833360df10eafe8f731bc35a9fe2f0f9
Ru-Two <darjirutu@gmail.com>
2020-10-31 17:07:27 -0400

    Updated DBTextAreaComponent

--- src/main/DBTextAreaComponent.java
+++ src/main/DBTextAreaComponent.java
@@ -2,33 +2,63 @@ package main;
 
 import javax.swing.*;
 import java.awt.*;
+import java.awt.event.*;
 
 
-public class DBTextAreaComponent extends JTextArea {
-
-    private Font font;
+public class DBTextAreaComponent extends JTextArea implements MouseListener, FocusListener {
 
     public DBTextAreaComponent(){
-        super("",4,30);
-        setLocation(0,0);
-        setSize(100,50);
-        font = new Font("Courier Regular", Font.PLAIN, 20);
+        super();
+
+        setBounds(0,0,50,50);
+
+        addMouseListener(this);
+        addFocusListener(this);
     }
 
     public DBTextAreaComponent(int x, int y, int width, int height){
         this();
-        setSize(width, height);
-        setLocation(x, y);
+        setBounds(x, y, width, height);
+        setRows((int)(getHeight()/getFont().getSize()));
+        setColumns((int)(getWidth()/getFont().getSize()));
     }
 
-    public void setFont(Font f){
-        font = f;
-        super.setFont(font);
+    public DBTextAreaComponent(Rectangle bounds){
+        this();
+        setBounds(bounds);
+        setRows((int)(getHeight()/getFont().getSize()));
+        setColumns((int)(getWidth()/getFont().getSize()));
     }
+
+    /*
+    public void actionPerformed(ActionEvent e){
+
+    }
+    */
+    public void mousePressed(MouseEvent e) {}
+
+    public void mouseReleased(MouseEvent e) {}
+
+    public void mouseEntered(MouseEvent e) {
+    }
+
+    public void mouseExited(MouseEvent e) {}
+
+    public void mouseClicked(MouseEvent e) {
+        //requestFocusInWindow();
+    }
+
+    public void focusGained(FocusEvent f) {
+
+    }
+
+    public void focusLost(FocusEvent f) {
+
+    }
+
 
     @Override
     public void paintComponent(Graphics g){
         super.paintComponent(g);
-        g.setFont(font);
     }
 }
