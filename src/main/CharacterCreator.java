b361ed10f767ba1509cbcec20ab092367b2f09b6
Ru-Two <darjirutu@gmail.com>
2020-11-01 00:37:05 -0400

    Almost finished Character Creator

--- src/main/CharacterCreator.java
+++ src/main/CharacterCreator.java
@@ -6,32 +6,28 @@ import javax.swing.Timer;
 import java.awt.event.*;
 import java.util.ArrayList;
 
+import static utilities.Utilities.RACE_OPTIONS;
+import static utilities.Utilities.CLASS_OPTIONS;
+import static utilities.PreloadedImages.*;
 
-public class CharacterCreator extends JFrame implements MouseListener, ActionListener {
+public class CharacterCreator extends JPanel implements MouseListener, ActionListener {
 
-    private static final String[] RACE_OPTIONS = {"Hill Dwarf", "Mountain Dwarf", "High Elf", "Wood Elf", "Dark Elf (Drow)",
-                                                  "Lightfoot Halfling", "Stout Halfling", "Human", "Dragonborn",
-                                                  "Forest Gnome", "Rock Gnome", "Half-Elf", "Half-Orc", "Tiefling"};
-
-    private static final String[] CLASS_OPTIONS = {"Barbarian", "Bard", "Cleric", "Druid", "Fighter", "Monk",
-                                                   "Paladin", "Ranger", "Rogue", "Sorcerer", "Warlock", "Wizard"};
-
-    private static final String[] ABILITY_SCORE_METHOD_OPTIONS = {"Dice Roll", "Manual Input"};
+    private static final String[] ABILITY_SCORE_METHOD_OPTIONS = {"Roll Ability Scores for me", "Manually Input Ability Scores"};
 
     private static final Font TITLE_FONT = new Font("Verdana", Font.PLAIN, 30);
     private static final Font TEXT_FONT = new Font("Verdana", Font.PLAIN, 20);
 
-    private Timer ticks_cc;
+    JTextArea player_name_tf, character_name_tf;
+    JComboBox race_selection, class_selection, ability_score_method_selection;
+    JButton done;
 
     private boolean running;
     private String player_name, character_name, race, cclass, ability_score_method;
+    private boolean ready;
 
     public CharacterCreator(){
-        super("Character Creation");
-
-        ticks_cc = new Timer(30, this);
-
-        running = true;
+        running = false;
+        ready = false;
 
         player_name = "";
         character_name = "";
@@ -39,49 +35,119 @@ public class CharacterCreator extends JFrame implements MouseListener, ActionLis
         cclass = "";
         ability_score_method = "";
 
-        initComponents();
+
 
         addMouseListener(this);
 
-        addWindowListener(new WindowAdapter() {
-            public void windowClosing(WindowEvent e) {
-                finish();
-            }
-        });
+        setSize(270, 520);
+        setLocation((int)((windowSize.getWidth() + sheetSize.getHeight() - sheetSize.getWidth() - getWidth())/2), (int)((windowSize.getHeight() - getHeight())/2));
 
-        setSize(500,300);
+        initComponents();
         setLayout(null);
-        setDefaultCloseOperation(DISPOSE_ON_CLOSE);
-        setResizable(false);
-        setVisible(true);
+        setVisible(false);
     }
 
     public void initComponents(){
-        JTextField player_name_tf = new JTextField();
-        player_name_tf.setSize(60, TEXT_FONT.getSize());
+        int len = 180;
 
-        JTextField character_name_tf = new JTextField();
-        character_name_tf.setSize(60, TEXT_FONT.getSize());
+        player_name_tf = new JTextArea();
+        player_name_tf.setBounds((int)((getWidth()-len)/2), 90, len, 30);
+        player_name_tf.setFont(new Font("Verdana", Font.PLAIN, 20));
 
-        JComboBox race_selection = new JComboBox(RACE_OPTIONS);
+        character_name_tf = new JTextArea();
+        character_name_tf.setBounds((int)((getWidth()-len)/2), 160, len, 30);
+        character_name_tf.setFont(new Font("Verdana", Font.PLAIN, 20));
 
+        race_selection = new JComboBox(RACE_OPTIONS);
+        race_selection.setSelectedIndex(0);
+        race_selection.setBounds((int)((getWidth()-len)/2), 230, len, 30);
+        race_selection.addActionListener(new ActionListener() {
+            public void actionPerformed(ActionEvent actionEvent) {
+                JComboBox tmp = (JComboBox)actionEvent.getSource();
+                race = "" + tmp.getSelectedIndex();
+            }
+        });
 
-        JComboBox class_selection = new JComboBox(CLASS_OPTIONS);
+        class_selection = new JComboBox(CLASS_OPTIONS);
+        class_selection.setSelectedIndex(0);
+        class_selection.setBounds((int)((getWidth()-len)/2), 300, len, 30);
+        class_selection.addActionListener(new ActionListener() {
+            public void actionPerformed(ActionEvent actionEvent) {
+                JComboBox tmp = (JComboBox)actionEvent.getSource();
+                cclass = "" + tmp.getSelectedIndex();
+            }
+        });
 
-        JComboBox ability_score_method_selection = new JComboBox(ABILITY_SCORE_METHOD_OPTIONS);
+        ability_score_method_selection = new JComboBox(ABILITY_SCORE_METHOD_OPTIONS);
+        ability_score_method_selection.setSelectedIndex(0);
+        ability_score_method_selection.setBounds((int)((getWidth()-len)/2), 370, len, 30);
+        ability_score_method_selection.addActionListener(new ActionListener() {
+            public void actionPerformed(ActionEvent actionEvent) {
+                JComboBox tmp = (JComboBox)actionEvent.getSource();
+                ability_score_method = "" + tmp.getSelectedIndex();
+            }
+        });
+
+        done = new JButton("Create");
+        done.setBounds((int)((getWidth()-100)/2), 430, 100, 30);
+        done.addActionListener(new ActionListener() {
+            public void actionPerformed(ActionEvent actionEvent) {
+                pullInfo();
+                ready = true;
+            }
+        });
+
+        add(done);
+        add(player_name_tf);
+        add(character_name_tf);
+        add(race_selection);
+        add(class_selection);
+        add(ability_score_method_selection);
     }
 
-    private void finish(){
-
+    public void start(){
+        setVisible(true);
         running = false;
+        ready = false;
+    }
+
+    public void finish(){
+        setVisible(false);
+        running = false;
+        ready = false;
+    }
+
+    public void pullInfo(){
+        player_name = player_name_tf.getText();
+        character_name = character_name_tf.getText();
     }
 
     public boolean isRunning() {
         return running;
     }
 
+    public boolean isReady(){
+        return ready;
+    }
+
     public String getInformation(){
         return player_name + "." + character_name + "." + race + "." + cclass + "." + ability_score_method;
+    }
+
+    public void paintComponent(Graphics g){
+        super.paintComponent(g);
+
+        g.setColor(Color.BLACK);
+        g.drawRect(0,0,getWidth()-1, getHeight()-1);
+
+        DungeonsBeyond.drawBox(player_name_tf, g);
+        DungeonsBeyond.drawBox(character_name_tf, g);
+
+        g.setFont(new Font("Verdana", Font.PLAIN, 20));
+        g.drawString("Player Name", 70, 85);
+        g.drawString("Character Name", 52, 155);
+        g.drawString("Race", 110, 225);
+        g.drawString("Class", 105, 295);
     }
 
     //ActionListener function
@@ -99,7 +165,6 @@ public class CharacterCreator extends JFrame implements MouseListener, ActionLis
 
     public void mouseExited(MouseEvent e) {}
 
-    public void mouseClicked(MouseEvent e) {
+    public void mouseClicked(MouseEvent e) {}
 
-    }
 }
