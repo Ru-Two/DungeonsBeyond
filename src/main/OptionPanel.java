d1c197e386f2b0b4b9a2cb48e0609483e6a0781f
Jacob Evangelista <jacob.evangelista0015@yahoo.ca>
2020-11-05 23:01:17 -0500

    Update OptionPanel.java

--- src/main/OptionPanel.java
+++ src/main/OptionPanel.java
@@ -8,7 +8,12 @@ import javax.swing.*;
 import java.awt.*;
 import java.awt.event.*;
 import java.awt.image.*;
+import java.io.File;
+import java.io.IOException;
+import java.nio.Buffer;
+import java.sql.SQLOutput;
 import java.util.ArrayList;
+import java.util.Scanner;
 
 public class OptionPanel extends JPanel implements MouseListener {
 
@@ -19,15 +24,11 @@ public class OptionPanel extends JPanel implements MouseListener {
         setSize(100,100);
         ArrayList<JButton> buttons = new ArrayList<JButton>();
         addMouseListener(this);
+
         buttons.add(addButton(10,10,80,10, "Save as PNG"));
-        /*---
-        need to correct dimensions for buttons
-        
         buttons.add(addButton(10,10,80,10, "Save as file"));
-        buttons.add(addButton((10,10,80,10, "Load file into new sheet"));
-        
-        
-        */
+        buttons.add(addButton(10,10,80,10, "Load file into new sheet"));
+
         //save as PNG
         buttons.get(0).addActionListener(new ActionListener() {
             @Override
@@ -39,27 +40,50 @@ public class OptionPanel extends JPanel implements MouseListener {
                 - panel of that sheet
                 - name generator method for file name of saved sheet maybe ex. output string "charactersheet1" 2, 3 and soforth.
                  or input file name
-                
-                 do we have to make a getter method for current sheet index in dungeonsBeyond class?
-                
+                - have a choice to what location to save the sheet.
+
+               do we have to make a getter method for current sheet index in dungeonsBeyond class?
+
                 DungeonsBeyond.allSheets.get(DungeonsBeyond.get(currentSheetIndex))
-                
-               Need to find filename  
-               BufferedImage image = new BufferedImage(frame.getWidth(), frame.getHeight(), BufferedImage.TYPE_INT_RGB); 
-               Graphics2D graphics2D = image.createGraphics(); 
-               frame.paint(graphics2D); 
-               ImageIO.write(image,"png", new File("image.png"));
-               
-               write(RenderedImage im, String formatName, File output)
-               
-               
-               
-               
+
+
                */
+
+                //Takes screen shot of sheet window
+                //TODO fix cropping to only display the character sheet
+                Container pic = getRootPane().getContentPane();
+                BufferedImage image = new BufferedImage((pic.getWidth()), pic.getHeight(), BufferedImage.TYPE_INT_RGB);
+                BufferedImage crop = image.getSubimage(0, 0 ,image.getWidth(),image.getHeight());
+                Graphics2D g = crop.createGraphics();
+                pic.paint(g);
+
+
+                JFrame parentFrame = new JFrame();
+                JFileChooser fileChooser = new JFileChooser();
+
+                //information for the save directory window
+                fileChooser.setDialogTitle("Specify a file to save");
+                int userSelection = fileChooser.showSaveDialog(parentFrame);
+
+                //If the user hits save
+                if (userSelection == JFileChooser.APPROVE_OPTION) {
+                    File fileToSave = fileChooser.getSelectedFile();
+                    System.out.println("Save as file: " + fileToSave.getAbsolutePath());
+
+                }
+
+                //Saves to this director
+                //fileChooser.getSelectedFile() = selected director and file name
+                try {
+                    ImageIO.write(crop,"png", fileChooser.getSelectedFile());
+                } catch (IOException ioException) {
+                    ioException.printStackTrace();
+                }
+
             }
         });
-        
-        // save as text file 
+
+        // save as text file
         buttons.get(1).addActionListener(new ActionListener() {
             @Override
             public void actionPerformed(ActionEvent e) {
@@ -67,23 +91,53 @@ public class OptionPanel extends JPanel implements MouseListener {
                 /* Have to use ImageIOWrite
                 what we need
                - saving characterinfo to txt file
-               
-               */
+                 */
+
+                JFrame frame = new JFrame();
+                JFileChooser fileChooser = new JFileChooser();
+                fileChooser.setDialogTitle("Specify a file to load");
+                int userSelection = fileChooser.showSaveDialog(frame);
+
+                if (userSelection == JFileChooser.APPROVE_OPTION){
+                    File selectedFile = fileChooser.getSelectedFile();
+                    System.out.println("Your Selected File: " + selectedFile.getAbsoluteFile());
+                }
+
             }
         });
-        
+
         // load a text file
         buttons.get(2).addActionListener(new ActionListener() {
             @Override
             public void actionPerformed(ActionEvent e) {
 
-                /* 
+                /*
                 what we need
                -loading characterinfo into a new sheet
-               */
+                */
+
+                JFrame frame = new JFrame();
+                JFileChooser fileChooser = new JFileChooser();
+                fileChooser.setDialogTitle("Specify a file to load");
+                int userSelection = fileChooser.showOpenDialog(frame);
+
+                try{
+                    if (userSelection == JFileChooser.APPROVE_OPTION) {
+                        File selectedFile = fileChooser.getSelectedFile();
+                        System.out.println("Your Selected File: " + selectedFile.getAbsoluteFile());
+
+                        Scanner s = new Scanner(selectedFile);
+
+                    }
+                }catch (IOException ioException) {
+                    ioException.printStackTrace();
+                }
+
+
+
             }
         });
-        
+
     }
 
     public OptionPanel(int x, int y, int wid, int hei){
