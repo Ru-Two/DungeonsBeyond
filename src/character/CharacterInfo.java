8564987bdace2bec54e0b34fe38922504627c938
Andrew Ferasol <ajferasol@gmail.com>
2020-11-09 23:35:40 -0500

    added get and set for savingthrows

--- src/character/CharacterInfo.java
+++ src/character/CharacterInfo.java
@@ -120,7 +120,18 @@ public class CharacterInfo {
         this.mergeRace();
         initiative = getMod(DEX);
     }
-
+    
+    public void setProfSavingThrows(CharacterClass c){
+        int[] temp = c.getSavingThrows();
+        int prof1 = temp[0];
+        int prof2 = temp[1];
+        savingThrows[prof1] = true;
+        savingThrows[prof2] = true;
+    }
+    public boolean[] getProfSavingThrows(){
+        return savingThrows;
+    }
+    
     public void setRaceAndClass(CharacterRace r, CharacterClass c){
         race = r;
         cclass = c;
