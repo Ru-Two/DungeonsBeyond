cdace68b541a88de8af0ef46ff5c64412628c929
Ru-Two <darjirutu@gmail.com>
2020-11-19 19:16:06 -0500

    Updated test case comments

--- tests/main/CharacterSheetTest.java
+++ tests/main/CharacterSheetTest.java
@@ -14,7 +14,7 @@ class CharacterSheetTest {
         CharacterSheet test = new CharacterSheet();
         Rectangle test_bounds = test.getRelativeBounds(0,0,100,100);
 
-        //doing the math by hand, the dimensions should be shifted to (0,0,46,46)
+        //doing the math by hand, the dimensions should be shifted to (0,0,46,46). So just check one random value, say width, to test case
         assertEquals(46, (int)test_bounds.getWidth());
     }
 }
--- tests/main/DungeonsBeyondTest.java
+++ tests/main/DungeonsBeyondTest.java
@@ -12,6 +12,9 @@ class DungeonsBeyondTest {
     //Testing of method by Ruturaj Darji
     @Test
     void getImage() {
+        //within the getImage function, there is already a print statement declaring if the image was successfully loaded or not
+        //if that statement prints, this test is passed
+
         Image test = DungeonsBeyond.getImage("src/graphics/TestImage.png");
     }
 }
